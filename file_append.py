529deb132380029d5f6e7104f0b5ada0a1893121
shivam5555 <will.shivam5555@gmail.com>
2025-03-01 16:15:01 +0530

    new files added devops_log file_append.py file_read.py

---
+++ file_append.py
@@ -0,0 +1,4 @@
+with open("devops_log.txt", "a") as file:
+    file.write("New Log Entry: Deployment Completed\n")
+
+print("New log added")
---
+++ file_read.py
@@ -0,0 +1,3 @@
+with open("devops_log.txt", "r") as file:
+    content = file.read()
+    print("File Content:\n", content);
